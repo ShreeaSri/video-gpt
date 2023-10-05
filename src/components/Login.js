00097e684032ffd67c5a683afd8313b6276f6391
Shreea Sri <shreeasri@gmail.com>
2023-10-05 16:44:00 +0530

    completed sign in and sign up logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,10 @@
 import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../utils/firebase";
 
 const Login = () => {
@@ -17,7 +20,7 @@ const Login = () => {
     //validation logic for form data
 
     const messgae = checkValidData(
-      name.current.value,
+      //   name.current.value,
       email.current.value,
       password.current.value
     );
@@ -28,19 +31,38 @@ const Login = () => {
     //sign in sign up logic
     if (!signIn) {
       //sign up logic
-      createUserWithEmailAndPassword(auth, email, password)
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
         .then((userCredential) => {
           // Signed up
           const user = userCredential.user;
-          // ...
+          console.log(user);
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          // ..
+          setErrorMessage(errorCode + "-" + errorMessage);
         });
     } else {
       //sign in logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
     }
   };
 
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,5 +1,5 @@
-export const checkValidData = (name, email, password) => {
-  const isNameValid = /^[A-Z][a-zA-Z '.-]*[A-Za-z][^-]$/.test(name);
+export const checkValidData = (email, password) => {
+  //   const isNameValid = /^[A-Z][a-zA-Z '.-]*[A-Za-z][^-]$/.test(name);
 
   const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
     email
@@ -8,7 +8,7 @@ export const checkValidData = (name, email, password) => {
   const isPasswordValid =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
-  if (!isNameValid) return "Name is not valid";
+  //   if (!isNameValid) return "Name is not valid";
   if (!isEmailValid) return "Email id is not valid";
   if (!isPasswordValid) return "password is not valid";
 
